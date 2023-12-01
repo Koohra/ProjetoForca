199ef80011abf248fbe4f8af6d6fca4e030d221d
Miguel Pereira de Sousa <miguel.pereira.12@live.com>
2023-12-01 12:30:58 -0300

    Mudanças complementares do código.

--- ForcaConsole/Usuario.cs
+++ ForcaConsole/Usuario.cs
@@ -10,11 +10,42 @@ namespace ForcaConsole
     {
         internal static void MetodoPrincipal(string categoriaSorteada, string palavraSorteada)
         {
-            char letra = ReceberLetra();
             char[] palavraEscondida = new char[palavraSorteada.Length];
             palavraEscondida = AlterarArray(palavraEscondida, palavraSorteada);
-            palavraEscondida = VerificarLetra(palavraEscondida, palavraSorteada, letra);
-            Console.WriteLine($"\n{string.Join("", palavraEscondida)}");
+            Console.WriteLine($"A categoria da palavra é: {categoriaSorteada} ");
+            List<char> letraGuardada = new List<char>();
+            int erro = 0;
+            while (true)
+            {
+                char letra = ReceberLetra();
+                if (palavraSorteada.Contains(letra))
+                {
+                    if (VerificaLetraRepetida(letra, letraGuardada)) 
+                    {
+                        Console.WriteLine("\nVocê ja inseriu essa Letra");
+
+                    }
+                    else
+                    {
+                        palavraEscondida = VerificarLetra(palavraEscondida, palavraSorteada, letra);
+                        if(!VerificarVitoria(palavraEscondida))
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    erro++;
+                    Console.WriteLine($"\nVoce errou {erro} vezes");
+                    if (Perdeu(erro))
+                        break;
+                }
+                
+             Console.WriteLine($"\n{string.Join("", palavraEscondida)}");
+
+            }
         }
         private static char[] AlterarArray(char[] palavraEscondida, string palavraSorteada)
         {
@@ -47,6 +78,37 @@ namespace ForcaConsole
             Console.Write("Insira uma letra: ");
             char letra = Console.ReadKey().KeyChar;
             return letra;
+        }
+        private static bool VerificarVitoria(char[] palavraEscondida)
+        {
+            if (palavraEscondida.Contains('_'))
+            {
+                return true;
+            }
+            string palavraFinal = new string(palavraEscondida);
+            Console.WriteLine($"\nVocê venceu parabens voce encontrou a palavra: {palavraFinal}");
+            return false;
+        }
+
+        private static bool Perdeu(int erro)
+        {
+            if (erro == 6)
+            {
+                return true;
+            }
+            return false;
+        }
+        private static bool VerificaLetraRepetida(char letra, List<char> letraGuardada)
+        {
+            if (letraGuardada.Contains(letra))
+            {
+                return true;
+            }
+            else
+            {
+                letraGuardada.Add(letra);
+                return false;
+            }
         }
     }
 }
